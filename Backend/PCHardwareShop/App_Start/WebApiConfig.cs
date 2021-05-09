3893437f4d158df3d2af695981571af607ef40ca
A H Zahin <azahinhasan@gmail.com>
2021-05-09 11:35:56 +0600

    fast commit

---
+++ Backend/PCHardwareShop/App_Start/WebApiConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Newtonsoft.Json.Serialization;
+
+namespace PCHardwareShop
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            /*config.Formatters.Remove(config.Formatters.XmlFormatter);*/
+
+            //it will disable xml request 
+            //by this we can disable any kind for request
+
+
+            // config.Formatters.JsonFormatter.SerializerSettings = new CamelCasePropertyNamesContractResolver();
+            // it will convart api output to pascale notation
+
+
+
+            //EnableCorsAttribute cors = new EnableCorsAttribute("www.fb.com,www.aiub", "Authorization,Accept", "GET,Post...");
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*"); //(domain,header,methord)
+            config.EnableCors(cors);
+        }
+    }
+}
---
+++ Backend/PCHardwareShop/Controllers/AllProductsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web;
+using PCHardwareShop.Models;
+
+namespace PCHardwareShop.Controllers
+{
+   // [RoutePrefix("")]
+    
+    
+    public class AllProductsController : ApiController
+    {
+        PcHardwareShopEntities2 context = new PcHardwareShopEntities2();
+
+        [Route("api/products/laptop"),HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(context.ProductCategoryLinks.Where(x => x.Category.cName == "Laptop").ToList());
+        }
+
+        [Route("api/products/laptop/{id}")]
+        public IHttpActionResult Put([FromBody]ProductCategoryLink data,int id)
+        {
+            data.ID = id;
+            context.Entry(data).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+            return Ok(data);
+        }
+
+        [Route("api/products/laptop/{id}"),HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            var data = context.ProductCategoryLinks.Find(id);
+            var dataProduct = context.Products.Find(data.pID);
+
+            context.ProductSpecifications.Remove(context.ProductSpecifications.Find(dataProduct.SpecificationID));
+            context.Products.Remove(dataProduct);
+            //context.Categories.Remove(context.Categories.Find(data.pCategory));
+            context.ProductCategoryLinks.Remove(data);
+            context.SaveChanges();
+            return Ok("Done");
+        }
+
+        [Route("api/products/laptop")]
+        public IHttpActionResult Post(ProductCategoryLink data)
+        {
+
+            context.ProductCategoryLinks.Add(data);
+            context.SaveChanges();
+            return Ok("Success");
+        }
+    }
+}
