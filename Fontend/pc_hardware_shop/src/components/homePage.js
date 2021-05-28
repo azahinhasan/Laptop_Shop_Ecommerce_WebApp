75ef27140aa852ef64cab5a9adbffe635244161a
A H Zahin <azahinhasan@gmail.com>
2021-05-28 23:14:39 +0600

    some picture add --home page

--- Fontend/pc_hardware_shop/src/components/homePage.js
+++ Fontend/pc_hardware_shop/src/components/homePage.js
@@ -2,7 +2,7 @@ import React, { useState,useEffect,useReducer, useRef,useMemo } from 'react';
 //useContext in the header
 import axios from 'axios';
 //import GoogleMapReact from 'google-map-react';
-
+import classes from './homePage.css';
 
 
 const HomePage = props =>{
@@ -11,8 +11,14 @@ const HomePage = props =>{
         const [ipAddressDetails, setIpAddressDetails] = useState("");
 
         return (
-          <div className="">
-              <p>Home</p>
+          <div className={classes.homePage}>
+              <img className={classes.smallPic} src={require('../Content/HomePage/apple-imac.jpg').default} />
+              <img className={classes.largePic} src={require('../Content/HomePage/vminipreorder.jpg').default} />
+              <img className={classes.smallPic} src={require('../Content/HomePage/lenovo-yoga-all-model-271x350.jpg').default} />
+              <br/>
+              <img className={classes.largePic} src={require('../Content/HomePage/ekwb.jpg').default} />
+              <img className={classes.smallPic} src={require('../Content/HomePage/bluetooth-speaker-web-banner.jpg').default} />
+              <img className={classes.smallPic} src={require('../Content/HomePage/iphone-12-cables-converter.jpg').default} />
           </div>
         );
     } 
