908c816aa65d47ba643160bfe7b08762dc5e77f7
A H Zahin <azahinhasan@gmail.com>
2021-05-28 23:38:07 +0600

    some picture add --home page

--- Fontend/pc_hardware_shop/src/components/homePage.js
+++ Fontend/pc_hardware_shop/src/components/homePage.js
@@ -12,6 +12,8 @@ const HomePage = props =>{
 
         return (
           <div className={classes.homePage}>
+              <h2>Welcome To MyMyLeptopShop</h2>
+              <br/>
               <img className={classes.smallPic} src={require('../Content/HomePage/apple-imac.jpg').default} />
               <img className={classes.largePic} src={require('../Content/HomePage/vminipreorder.jpg').default} />
               <img className={classes.smallPic} src={require('../Content/HomePage/lenovo-yoga-all-model-271x350.jpg').default} />
@@ -19,6 +21,18 @@ const HomePage = props =>{
               <img className={classes.largePic} src={require('../Content/HomePage/ekwb.jpg').default} />
               <img className={classes.smallPic} src={require('../Content/HomePage/bluetooth-speaker-web-banner.jpg').default} />
               <img className={classes.smallPic} src={require('../Content/HomePage/iphone-12-cables-converter.jpg').default} />
+              <br/>
+              <img className={classes.smallPic} src={require('../Content/HomePage/gamingchairs.jpg').default} />
+              <img className={classes.smallPic} src={require('../Content/HomePage/mi-tvs.jpg').default} />
+              <img className={classes.largePic} src={require('../Content/HomePage/studio-equipments-web-banner.jpg').default} />
+              <br/>
+              <img className={classes.smallPic} src={require('../Content/HomePage/lenovo-yoga-all-model-271x350.jpg').default} />
+              <img className={classes.largePic} src={require('../Content/HomePage/dellinsp155000.jpg').default} />
+              <img className={classes.smallPic} src={require('../Content/HomePage/iphone-12-cables-converter.jpg').default} />
+              <br/>
+              <img className={classes.largePic} src={require('../Content/HomePage/vminipreorder.jpg').default} />
+              <img className={classes.smallPic} src={require('../Content/HomePage/M8-dual-arm.jpg').default} />
+              <img className={classes.smallPic} src={require('../Content/HomePage/bluetooth-speaker-web-banner.jpg').default} />
           </div>
         );
     } 
