c470d83c2f0fc88e1f54da4f8dd2dd432a710807
A H Zahin <azahinhasan@gmail.com>
2021-06-10 22:29:29 +0600

    Employee can change Orders status and see all Orders

--- Fontend/pc_hardware_shop/src/employeePart/allOrders.js
+++ Fontend/pc_hardware_shop/src/employeePart/allOrders.js
@@ -1,8 +1,9 @@
 import React,{ useState,useEffect} from 'react';
 import classes from './employee.css';
-import {Route,Switch,withRouter,Redirect,useParams,useHistory } from 'react-router-dom';
+import {Route,Switch,Link,Redirect,useParams,useHistory } from 'react-router-dom';
 import axios from '../api/axios';
-
+import moment from 'moment'
+//npm i moment --save //used for print only time or data
 const AllOrders = props =>{
 
         const [allOrders,setAllOrders]=useState([]);
@@ -13,15 +14,30 @@ const AllOrders = props =>{
                 console.log(r.data,"Orders");
             })
             
-        }, [])
+        }, []);
+
+  
+            
         return (
         <div  className={classes.Product}>
             <p>All Oeders</p>
-            <table>
+
+            <table className={classes.table}>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Order Date</th>
+                    <th>Status</th>
+                    <th>Deatils</th>
+                </tr>
             {allOrders.map(data=>{
             return( 
-            <tr>
+                <tr>
+                    <td>{data.ID}</td>
                     <td>{data.Name}</td>
+                    <td>{moment(data.OrderedData).format('MMMM Do YYYY')}</td>
+                    <td>{data.Status}</td>
+                    <td><Link>go</Link></td>
                 </tr>
                 )
             })}
