3389d8a8dfd7fa572a60ac8343c041e213ef0254
A H Zahin <azahinhasan@gmail.com>
2021-06-17 11:45:29 +0600

    Some server side chnage done

---
+++ Backend/PCHardwareShop/Models/MetaData/EmployeeInfoMetaData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.Xml.Serialization;
+
+namespace PCHardwareShop.Models.MetaData
+{
+    public class EmployeeInfoMetaData
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+        public int LoginTableID { get; set; }
+        public Nullable<int> RankTableID { get; set; }
+
+        public virtual EmployeeRank EmployeeRank { get; set; }
+
+        [JsonIgnore, XmlIgnore]
+        public virtual UserLoginTable UserLoginTable { get; set; }
+    }
+}
---
+++ Backend/PCHardwareShop/Models/MetaData/EmployeeRankMetaData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.Xml.Serialization;
+
+namespace PCHardwareShop.Models.MetaData
+{
+    public class EmployeeRankMetaData
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public EmployeeRankMetaData()
+        {
+            this.EmployeeInfoes = new HashSet<EmployeeInfo>();
+        }
+
+        public int ID { get; set; }
+        public string Rank { get; set; }
+        public string Employee { get; set; }
+        public string Products { get; set; }
+        public string Orders { get; set; }
+        public string Others { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+
+        [JsonIgnore, XmlIgnore]
+        public virtual ICollection<EmployeeInfo> EmployeeInfoes { get; set; }
+    }
+}
